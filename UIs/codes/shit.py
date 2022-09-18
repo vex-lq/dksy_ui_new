8dd16963746dc3e4864a495df61e476580278c80
reset <583316119@qq.com>
2022-09-18 15:48:11 +0800

    shit.py回滚修改测试

--- UIs/codes/shit.py
+++ UIs/codes/shit.py
@@ -5,3 +5,7 @@ from Global_Varibles import *
 for root, dir, file in os.walk('F:\_____科创作业汇总\上传作业电子版\__$_尚丰', topdown=False):
     if root.split("\\") in All_Classified_Folders_Name_List:
         print(root, dir)
+
+
+#修改测试
+#################
